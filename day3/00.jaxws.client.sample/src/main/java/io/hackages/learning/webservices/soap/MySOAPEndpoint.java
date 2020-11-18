8e3dc020f8a2dcbd8c52beedb6fba64a306b508d
Tata DA SILVEIRA <dasilveira.tata@gmail.com>
2020-11-18 06:55:17 +0100

    added day3 sources

---
+++ day3/00.jaxws.client.sample/src/main/java/io/hackages/learning/webservices/soap/MySOAPEndpoint.java
@@ -0,0 +1,21 @@
+package io.hackages.learning.webservices.soap;
+
+import javax.jws.WebService;
+
+
+@WebService
+public class MySOAPEndpoint {
+
+
+	/**
+	 * Define the service here
+	 */
+	/*
+	public SampleResponse sampleService(SampleRequest request) {
+		SampleResponse response = new SampleResponse();
+		// Process the funtionnality
+		return response;
+	}
+	*/
+
+}
---
+++ day3/00.jaxws.client.sample/src/main/java/io/hackages/learning/webservices/soap/SOAPServiceClient.java
@@ -0,0 +1,28 @@
+package io.hackages.learning.webservices.soap;
+
+/*
+import io.hackages.learning.webservices.soap.stubs.HelloEndpoint;
+import io.hackages.learning.webservices.soap.stubs.HelloEndpointService;
+import io.hackages.learning.webservices.soap.stubs.HelloRequest;
+import io.hackages.learning.webservices.soap.stubs.HelloResponse;
+*/
+import java.util.logging.Logger;
+
+public class SOAPServiceClient {
+	
+	public static void main(String[] args) {
+		/*
+		HelloEndpointService serviceClient = new HelloEndpointService();
+		HelloEndpoint port = serviceClient.getHelloEndpointPort();
+		
+		HelloRequest request = new HelloRequest();
+		request.setName("Voldemort");
+		
+		HelloResponse response = port.sayHello(request);
+		
+		Logger.getAnonymousLogger().info("The response is: "+response.getMessage());
+		*/
+		
+	}
+
+}
